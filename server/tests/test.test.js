14ba84beb40ee3a1ce8c3e891ab56bc6d8d28e95
mjjstockman <mjjstockman@gmail.com>
2024-12-05 15:29:36 +0000

    add failing test for SuperTest set up

--- server/tests/test.test.js
+++ server/tests/test.test.js
@@ -2,4 +2,11 @@ describe('Testing setup', () => {
   it('runs Jest correctly', () => {
     expect(true).toBe(true);
   });
+
+  it('returns 200 for GET /api/test endpoint', async () => {
+    const response = await request('http://localhost:5001/api/test').get(
+      '/api/test'
+    );
+    expect(response.statusCode).toBe(200);
+  });
 });
